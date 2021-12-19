f12db2ecc2caddb6e115e30ebb82b521d9141ba5
Danperad <kokorin0521@gmail.com>
2021-12-19 16:05:36 +0300

    finish lab_13

---
+++ src/main/java/com/vyatsu/lab/OrderService.java
@@ -0,0 +1,25 @@
+package com.vyatsu.lab;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+    private Cart cart;
+
+    @Autowired
+    public void setCart(Cart c){
+        this.cart = c;
+    }
+
+    public void form(){
+        System.out.println("\nВаши покупки:");
+        int sum = 0;
+        for (Product p: cart.getProducts()) {
+            System.out.println(p.getTitle());
+            sum+=p.getCost();
+        }
+        System.out.println("Итоговая стоимость: "+ sum);
+    }
+}
---
+++ src/main/java/com/vyatsu/lab/Product.java
@@ -0,0 +1,26 @@
+package com.vyatsu.lab;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Product {
+    @Getter
+    @Setter
+    private int ID;
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private int cost;
+    public Product(int id, String title, int cost){
+        this.ID = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Product(){
+    }
+}
